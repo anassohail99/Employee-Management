394d37b45b052eb7586aabc300a873cfbf901336
anassohail99 <anas.sohailabs1@gmail.com>
2020-07-08 12:55:30 +0500

    Home Component Finalized

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Heading } from "./Heading";
+import { Employeelist } from "./EmployeeList";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <Fragment>
+      <div className="App">
+        <div className="container mx-auto">
+          <h3 className="text-center  text-3xl mt-20 text-base leading-8 text-black font-bold tracking-wide uppercase">
+            CRUD with React Context API and Hooks
+          </h3>
+          <Heading />
+          <Employeelist />
+        </div>
+      </div>
+    </Fragment>
+  );
 };
-
 export default Home;
